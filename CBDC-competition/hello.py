76161c1ca061bad5743f199cea918fb6f5c37200
Mueangapi <104725034+Mueangapi@users.noreply.github.com>
2024-03-18 22:16:48 +0700

    Add files via upload

---
+++ CBDC-competition/hello.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="🍁",
+)
+
+st.write("# Welcome to CBDC network analysis tool 👋")
+
+st.sidebar.success("Let's try")
+
+st.markdown(
+    """
+    Thailand will not be the same anymore! 
+""" 
+)
